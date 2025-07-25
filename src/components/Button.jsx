06563780b5afdec1ec179dd532909101a8847047
boshraemad <boshraemad2004@icloud.com>
2025-07-25 18:44:46 +0300

    customize button color

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
 export default function Button({onclick , color , children}) {
+
   return (
-    <button className={`inline-block shrink-0 rounded-md border bg-${color} px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-indigo-600 focus:ring-3 focus:outline-hidden `}>
+    <button className={`inline-block shrink-0 rounded-md ${color} px-12 py-3 text-sm font-medium text-white transition focus:ring-3 focus:outline-hidden `}>
         {children}
     </button>
   )
--- src/features/auth/components/Footer.jsx
+++ src/features/auth/components/Footer.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom"
 export default function Footer({register}) {
   return (
     <footer className="col-span-6 sm:flex sm:items-center sm:gap-4">
-    <Button color="sunset">Continue</Button>
-    <p className="mt-4 text-sm text-gray-500 sm:mt-0 font-[600] cursor-pointer">
+    <Button color="bg-sunset" text="text-turquoise">Continue</Button>
+    <p className="inline-block ms-2 mt-4 text-sm text-gray-500 sm:mt-0 font-[600] cursor-pointer">
         {
             !register ?  <Link to="/register" className="text-darkturquoise underline">
             Dont have account yet?
