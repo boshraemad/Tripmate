7cd986d7524442b7366f6a816358ad45d6e4075b
boshraemad <boshraemad2004@icloud.com>
2025-07-25 18:43:11 +0300

    change header component title

--- src/features/auth/components/Header.jsx
+++ src/features/auth/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header>
         <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-        Welcome to <span className="inline-block first-letter:text-sunset">Tripary</span>
+        Welcome to <span className="inline-block first-letter:text-sunset">Tripmate</span>
         </h1>
 
       <p className="mt-4 leading-relaxed text-gray-500">
