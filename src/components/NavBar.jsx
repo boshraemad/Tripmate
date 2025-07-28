56c852fee8274bb80a88d52c0eb0519d7a47d857
boshraemad <boshraemad2004@icloud.com>
2025-07-28 20:35:38 +0300

    fix navbar active links issue

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,6 +5,7 @@ import Button from "./Button";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { initFlowbite } from "flowbite";
+import { isAction } from "@reduxjs/toolkit";
 
 
 export default function NavBar() {
@@ -47,28 +48,29 @@ export default function NavBar() {
           </li>
         </ul>
       </div>
-      <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
+    </> : <Button color="bg-turquoise" onClick={()=>navigate("/login")}>sign in</Button>
+    
+    }
+          <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
     </button>
-    </> : <Button color="bg-turquoise" onClick={()=>navigate("/login")}>sign in</Button>
-    }
   </div>
   <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent">
       <li>
-        <NavLink to="/" className={`navlink ${({isActive})=> isActive? "activenavlink " : "unactivenavlink"}`}>Home</NavLink>
+        <NavLink to="/"  className={({ isActive }) => isActive ? "navlink activenavlink" : "navlink unactivenavlink"}>Home</NavLink>
       </li>
       <li>
-        <NavLink to="/discover" className={`navlink ${({isActive})=> isActive? "activenavlink " : "unactivenavlink"}`}>Discover</NavLink>
+        <NavLink to="/discover" className={({ isActive }) => isActive ? "navlink activenavlink" : "navlink unactivenavlink"}>Discover</NavLink>
       </li>
       <li>
-        <NavLink to="/trips" className={`navlink ${({isActive})=> isActive? "activenavlink " : "unactivenavlink"}`}>Trips</NavLink>
+        <NavLink to="/trips" className={({ isActive }) => isActive ? "navlink activenavlink" : "navlink unactivenavlink"}>Trips</NavLink>
       </li>
       <li>
-        <NavLink to="/userReview" className={` navlink ${({isActive})=> isActive? "activenavlink " : "unactivenavlink"}`}>Review</NavLink>
+        <NavLink to="/userReview" className={({ isActive }) => isActive ? " navlink activenavlink" : "navlink unactivenavlink"}>Review</NavLink>
       </li>
     </ul>
   </div>
